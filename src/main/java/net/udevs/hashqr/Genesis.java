c6d33829f2acfbc9dd7293802106732ba30fe3e1
Sujal <ss6sujal@gmail.com>
2017-12-27 00:05:12 +0530

    added useful comments about why treemap is used in the params.

--- src/main/java/net/udevs/hashqr/Genesis.java
+++ src/main/java/net/udevs/hashqr/Genesis.java
@@ -3,5 +3,11 @@ package net.udevs.hashqr;
 import java.util.TreeMap;
 
 public interface Genesis {
+	
+	/*	
+	 *  Treemap is needed in order to keep metadata autosorted so that the hasher does
+	 *	not produce different hashes when same metadata appear in different order.
+	 *
+	*/
 	Seed createRandomSeed(TreeMap<String,String> metadata);
 }
